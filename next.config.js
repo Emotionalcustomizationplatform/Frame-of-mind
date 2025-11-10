fbde0083680a539310f2ce175bb60f52f0b1f7db
Emotionalcustomizationplatform <dpx204825@gmail.com>
2025-11-11 05:57:00 +0800

    更新 next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
-          { key: 'Permissions-Policy', value: 'geolocation=()' }
-        ]
-      }
-    ];
-  }
-}
+  experimental: { appDir: true },
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        { key: "X-Frame-Options", value: "DENY" },
+        { key: "X-Content-Type-Options", value: "nosniff" },
+        { key: "Referrer-Policy", value: "no-referrer-when-downgrade" }
+      ]
+    }
+  ]
+};
 module.exports = nextConfig;
